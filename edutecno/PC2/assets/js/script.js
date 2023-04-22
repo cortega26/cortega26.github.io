30c84ad723ba1622b220d48b5aa0b25f44ad7e2a
Carlos Ortega <cortega77@gmail.com>
2023-04-21 22:29:00 -0400

    Se añaden archivos faltantes

---
+++ edutecno/PC2/assets/js/script.js
@@ -0,0 +1,32 @@
+const apiUrl = 'https://digimon-api.vercel.app/api/digimon';
+
+const digimonList = document.querySelector('.digimon-list ul');
+const digimonDetails = document.querySelector('.digimon-details .digimon');
+
+// Le hacemos fetch a la API y mostramos la lista de Digimons
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    try {
+      data.sort((a, b) => (a.name > b.name) ? 1 : -1);
+      for (let digimon of data) {
+        const li = document.createElement('li');
+        li.textContent = digimon.name;
+        li.addEventListener('click', () => {
+          showDigimonDetails(digimon);
+        });
+        digimonList.appendChild(li);
+      }
+    } catch(error) {
+      console.error(error);
+    }
+  });
+
+// Muestra los detalles de un Digimon en específico
+function showDigimonDetails(digimon) {
+  digimonDetails.innerHTML = `
+    <h3>${digimon.name}</h3>
+    <img src="${digimon.img}" alt="${digimon.name}">
+    <p><strong>Nivel:</strong> ${digimon.level}</p>
+  `;
+}
